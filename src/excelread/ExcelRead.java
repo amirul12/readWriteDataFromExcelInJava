582d0c5fd8e37fcfc17a6464686de3bcbc0c852d
Amirul Islam <amirul.csejust@gmail.com>
2016-10-07 21:50:30 +0600

    successfully run

--- src/excelread/ExcelRead.java
+++ src/excelread/ExcelRead.java
@@ -6,6 +6,7 @@
 package excelread;
 
 import java.io.File;
+import jxl.Cell;
 import jxl.Sheet;
 import jxl.Workbook;
 
@@ -20,10 +21,23 @@ public class ExcelRead {
      */
     public static void main(String[] args) throws Exception{
         // TODO code application logic here
-        File f = new File("");
+        File f = new File("D:\\data1.xls");
         
         Workbook wb = Workbook.getWorkbook(f);
         Sheet s= wb.getSheet(0);
+        int row = s.getRows();
+        int col = s.getColumns();
+        
+        for (int i = 0; i < row; i++) {
+            
+            for (int j = 0; j < col; j++) { 
+                Cell c = s.getCell(j, i);
+                System.out.print(c.getContents()+"\t");
+                
+            }
+            System.out.println("");
+            
+        }
         
     }
     
