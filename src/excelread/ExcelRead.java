df7fced825d9afa83e6519dd1a4c2b46236631d3
Amirul Islam <amirul.csejust@gmail.com>
2016-10-08 08:35:37 +0600

    adding counting one value

--- src/excelread/ExcelRead.java
+++ src/excelread/ExcelRead.java
@@ -6,6 +6,9 @@
 package excelread;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 import jxl.Cell;
 import jxl.Sheet;
 import jxl.Workbook;
@@ -21,24 +24,36 @@ public class ExcelRead {
      */
     public static void main(String[] args) throws Exception{
         // TODO code application logic here
-        File f = new File("D:\\data1.xls");
-        
+        File f = new File("D:\\data1.xls"); // data source
+        List<String> list = new ArrayList<>();
         Workbook wb = Workbook.getWorkbook(f);
         Sheet s= wb.getSheet(0);
         int row = s.getRows();
         int col = s.getColumns();
-        
-        for (int i = 0; i < row; i++) {
-            
-            for (int j = 0; j < col; j++) { 
+   
+        for (int i = 1; i < row; i++) {
+                 int countt=0;
+            for (int j = 1; j < col; j++) { 
                 Cell c = s.getCell(j, i);
+                
+                String st=c.getContents();
                 System.out.print(c.getContents()+"\t");
+                list.add(st);
+              //  System.out.println(st);
+                if (st.equals("1")) {
+                    countt++;
+                     
+                }
                 
             }
+           System.out.println("Item List"+i+":"+countt);
             System.out.println("");
+           
             
+          
         }
-        
-    }
-    
+            
+           
+        }
+           
 }
